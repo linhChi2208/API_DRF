249d8fa63d5d64a8fe8448c48c4b64a375a83e57
HOANG LINH CHI <hoanglinhchi2208ftu@gmail.com>
2022-04-28 15:29:00 +0200

    update equipements test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -14,9 +15,22 @@ class EquipementsTestCase(APITestCase):
     self.user = User.objects.create_user(username="example",password = "password123")
     self.token = Token.objects.get(user__username="example")
     self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
-    self.categories = models.Categories.objects.create(name="electronics", slug="electronics", description="electric device")
+    
+    self.categories = models.Categories.objects.create(name="electronics", slug="electronics", description="electric device", parent=None, photo=None)
 
 
   def test_equipements_list(self):
+    """Test API to get all the list"""
     response = self.client.get(reverse('EquipementsList'))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_equipements_create(self):
+    """Test to create new equipment""" 
+    data = {
+      "name": "printer",
+      "slug": "printer",
+      "categories": self.categories,
+      "quantity": 10
+    }
+    response = self.client.post(reverse('EquipementsList'), data)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
